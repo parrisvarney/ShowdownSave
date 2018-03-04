22e6973830e9fcd9e0e5b829fba77989ea264445
Parris Varney <parris@bedrockdata.com>
2018-03-04 14:27:36 -0500

    Added save/load functionality from extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,13 +1,53 @@
 // Load your teams
-chrome.storage.sync.get(null, (options) => {
-    $.ajax({
-        "url": `https://8duqbw8p14.execute-api.us-east-1.amazonaws.com/one/team?username=${options.username}&password=${options.password}`,
-        "method": "GET",
-    }).done(function (teams) {
-        for (let team of teams) {
-            const row = `<li>${team}</li>`;
-            $("#teams-list").append(row);
+$('document').ready(() => {
+    chrome.storage.sync.get(null, (options) => {
+        $.ajax({
+            "url": `https://8duqbw8p14.execute-api.us-east-1.amazonaws.com/one/teams?username=${options.username}&password=${options.password}`,
+            "method": "GET",
+        }).done(function (teams) {
+            for (let team of teams) {
+                const row = `<tr><td>${team}</td><td><a class="import-team-link link" data-team="${team}">Copy to clipboard</a></td></tr>`;
+                $("#teams-table").append(row);
+            }
+        });
+    });
+
+    chrome.runtime.onMessage.addListener(function (request, sender) {
+        if (request.action == "getSource") {
+            const source = $(request.source);
+            $("#save-team-name").val(source.find('.teamnameedit').val());
+            $("#save-team-contents").val(source.find('.teamedit').text());
         }
+    });
+
+    chrome.tabs.executeScript(null, {
+        file: "showdown-page.js"
+    }, function () {
+        if (chrome.runtime.lastError) {
+            // $("#save-team-contents").val('There was an error loading your team script : ' + chrome.runtime.lastError.message);
+        }
+    });
+});
+
+$("#teams-table").on('click', '.import-team-link', (data) => {
+    const teamName = $(data.target).data('team');
+    chrome.storage.sync.get(null, (options) => {
+        $.ajax({
+            "url": `https://8duqbw8p14.execute-api.us-east-1.amazonaws.com/one/team?username=${options.username}&password=${options.password}&team=${teamName}`,
+            "method": "GET",
+        }).done(function (teamData) {
+            console.log(teamData);
+            const clipboard = document.createElement("textarea");
+            clipboard.textContent = teamData;
+
+            const body = document.getElementsByTagName('body')[0]
+            body.appendChild(clipboard);
+
+            clipboard.select();
+            document.execCommand('copy');
+
+            body.removeChild(clipboard);
+        });
     });
 });
 
@@ -21,8 +61,8 @@ $('#save-team-button').on('click', () => {
             "data": JSON.stringify({
                 "username": options.username,
                 "password": options.password,
-                "team":     $("#save-team-name").val(),
-                "data":     $("#save-team-contents").val(),
+                "team": $("#save-team-name").val(),
+                "data": $("#save-team-contents").val(),
             })
         })
     })
